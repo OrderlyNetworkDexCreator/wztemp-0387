a89cced52157a87b7c63aaa4e0cd6926ad393dc3
orderly-guoqing <guoqing@orderly.network>
2026-07-11 22:26:01 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_ID": "otter_dex",
-  "VITE_ORDERLY_BROKER_NAME": "Otter_lee",
+  "VITE_ORDERLY_BROKER_NAME": "Otter_lee_abc",
   "VITE_BROKER_EOA_ADDRESS": "0xd7bc97f0edb3b04e1604a546cef52f9ff4edfe71",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
@@ -14,7 +14,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_DISABLE_EVM_WALLETS": "false",
   "VITE_DISABLE_SOLANA_WALLETS": "false",
   "VITE_WALLETCONNECT_PROJECT_ID": "",
-  "VITE_APP_NAME": "Otter_lee",
+  "VITE_APP_NAME": "Otter_lee_abc",
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
